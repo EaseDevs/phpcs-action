e2ef5b035802d3b320d466463cf93fbbccc020ce
Steven Michaels <steven.michaels@gmail.com>
2020-09-02 11:29:49 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,17 @@ cp /action/problem-matcher.json /github/workflow/problem-matcher.json
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
 if [ -n "${INPUT_INSTALLED_PATHS}" ]; then
-    /phpcs --config-set installed_paths "${INPUT_INSTALLED_PATHS}"
+    ${INPUT_PHPCS_BIN_PATH} --config-set installed_paths "${INPUT_INSTALLED_PATHS}"
 fi
 
 if [ -z "${INPUT_ENABLE_WARNINGS}" ] || [ "${INPUT_ENABLE_WARNINGS}" = "false" ]; then
     echo "Check for warnings disabled"
 
-    /phpcs $GITHUB_WORKSPACE -n --report=checkstyle -d memory_limit=1024M
+    ${INPUT_PHPCS_BIN_PATH} $GITHUB_WORKSPACE -n --report=checkstyle -d memory_limit=1024M
 else
     echo "Check for warnings enabled"
 
-    /phpcs $GITHUB_WORKSPACE --report=checkstyle -d memory_limit=1024M
+    ${INPUT_PHPCS_BIN_PATH} $GITHUB_WORKSPACE --report=checkstyle -d memory_limit=1024M
 fi
 
 status=$?
